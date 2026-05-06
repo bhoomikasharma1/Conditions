e7fc027c85b019c06a571458b4446de31ea66029
Bhoomika Sharma <bhoomikasharma000@gmail.com>
2026-05-06 12:03:39 +0530

    add sets

---
+++ lists.py
@@ -0,0 +1,5 @@
+# define a lists of names
+names=["Herry", "john","josh","ginny"]
+names.append("dacco")
+names.sort()
+print(names)
--- sequences.py
+++ sequences.py
@@ -1,2 +1,8 @@
 names=["Herry", "john","josh"]
 print(names[1])
+
+coordianateX= 10.0
+coordianateY= 20.0
+
+coordianate = (10.0, 20.0)
+print(coordianate[0])
